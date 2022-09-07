c571d92931586b01420f4bd60b052d90a0f7f11e
margo-dubovik <margodubovik531@gmail.com>
2022-09-07 14:32:15 +0300

    modified models

--- djangoweatherreminder/weather/models.py
+++ djangoweatherreminder/weather/models.py
@@ -16,21 +16,18 @@ class CityWeather(models.Model):
     city = models.ForeignKey(CityName, on_delete=models.CASCADE, related_name="weather")
     last_info_update = models.DateTimeField(auto_now=True)
     weather_description = models.CharField(max_length=200)
-    temperature = models.IntegerField()
-    feels_like = models.IntegerField()
-    humidity = models.IntegerField()
-    pressure = models.IntegerField()
-    visibility = models.IntegerField()
-    wind_speed = models.IntegerField()
-    clouds = models.IntegerField()
-    rain = models.IntegerField()
-    snow = models.IntegerField()
+    temperature = models.FloatField()
+    feels_like = models.FloatField()
+    humidity = models.FloatField()
+    pressure = models.FloatField()
+    visibility = models.FloatField()
+    wind_speed = models.FloatField()
+    clouds = models.FloatField()
+    rain = models.FloatField()
+    snow = models.FloatField()
 
     def __str__(self):
         return f"city {self.city}, weather: {self.weather_description}, {self.temperature}°C, " \
-               f"feels_like {self.feels_like}°C, humidity {self.humidity}%, pressure {self.pressure}hPa," \
-               f"visibility {self.visibility}m, wind speed {self.wind_speed}m/sec, cloudiness {self.clouds} %," \
-               f"rain volume for the last 1 hour {self.rain}mm, snow volume for the last 1 hour {self.snow}mm," \
                f" last update on {self.last_info_update}"
 
 
@@ -42,5 +39,5 @@ class UserSubscription(models.Model):
     last_info_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"user {self.user}, city {self.city}, notify every {self.notification_frequency}h, " \
-               f"weather_info {self.weather_info}, last update on {self.last_info_update}"
+        return f"user {self.user}, city {self.city.name}, notify every {self.notification_frequency}h, " \
+               f"last update on {self.last_info_update}"
