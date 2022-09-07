e99cf8b0769ad14f002b379c64c13022aeda1a59
margo-dubovik <margodubovik531@gmail.com>
2022-09-07 14:17:11 +0300

    modified models fields

--- djangoweatherreminder/weather/models.py
+++ djangoweatherreminder/weather/models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 
 class CityName(models.Model):
-    city_id = models.IntegerField()
-    city_name = models.CharField(max_length=150)
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=150)
     country = models.CharField(max_length=150)
     region = models.CharField(max_length=150)
 
     def __str__(self):
-        return f"{self.city_name}, {self.region}, {self.country}"
+        return f"{self.name}, {self.region}, {self.country}"
 
 
 class CityWeather(models.Model):
-    city_id = models.ForeignKey(CityName, on_delete=models.CASCADE, related_name="weather")
+    city = models.ForeignKey(CityName, on_delete=models.CASCADE, related_name="weather")
     last_info_update = models.DateTimeField(auto_now=True)
     weather_description = models.CharField(max_length=200)
     temperature = models.IntegerField()
@@ -27,7 +27,7 @@ class CityWeather(models.Model):
     snow = models.IntegerField()
 
     def __str__(self):
-        return f"city {self.city_id}, weather: {self.weather_description}, {self.temperature}°C, " \
+        return f"city {self.city}, weather: {self.weather_description}, {self.temperature}°C, " \
                f"feels_like {self.feels_like}°C, humidity {self.humidity}%, pressure {self.pressure}hPa," \
                f"visibility {self.visibility}m, wind speed {self.wind_speed}m/sec, cloudiness {self.clouds} %," \
                f"rain volume for the last 1 hour {self.rain}mm, snow volume for the last 1 hour {self.snow}mm," \
@@ -35,11 +35,12 @@ class CityWeather(models.Model):
 
 
 class UserSubscription(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subscriptions")
-    city_id = models.ForeignKey(CityWeather, on_delete=models.CASCADE, related_name="subscriptions")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subscriptions")
+    city = models.ForeignKey(CityName, on_delete=models.CASCADE, related_name="subscriptions")
+    weather_info = models.ForeignKey(CityWeather, on_delete=models.CASCADE, related_name="subscriptions", null=True)
     notification_frequency = models.IntegerField()
     last_info_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"user {self.user_id}, city {self.city_id}, notify every {self.notification_frequency}h, " \
-               f"last update on {self.last_info_update}"
+        return f"user {self.user}, city {self.city}, notify every {self.notification_frequency}h, " \
+               f"weather_info {self.weather_info}, last update on {self.last_info_update}"
