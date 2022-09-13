d762c869dd29b94c123b755113749108d74b2b0b
margo-dubovik <margodubovik531@gmail.com>
2022-09-13 18:23:01 +0300

    auth tested

--- djangoweatherreminder/users/tests/test_users.py
+++ djangoweatherreminder/users/tests/test_users.py
@@ -21,4 +21,11 @@ def test_registration(api_client):
     assert response_json['res'] == 'Registered successfully'
     assert response_json['user info'] == {'email': data['email']}
 
-
+@pytest.mark.skip
+@pytest.mark.django_db
+def test_delete_account(api_client_with_authenticated_user):
+    url = reverse('delete_account')
+    response = api_client_with_authenticated_user.delete(url)
+    assert response.status_code == 200
+    response_json = json.loads(response.content)
+    assert response_json['result'] == 'user deleted'
